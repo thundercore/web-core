155dc27ee67aa2e68ea05036e5244846bc1f68e0
nick8319 <zhiltsov.nick@gmail.com>
2023-01-24 13:41:12 +0300

    feat: ThunderCore chain support

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,8 +3,9 @@ import chains from './chains'
 export const IS_PRODUCTION = process.env.NEXT_PUBLIC_IS_PRODUCTION
 
 export const GATEWAY_URL_PRODUCTION =
-  process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://safe-client.safe.global'
-export const GATEWAY_URL_STAGING = process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://safe-client.staging.5afe.dev'
+  process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://gateway.staging.safe.thundercore.com'
+export const GATEWAY_URL_STAGING =
+  process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://gateway.staging.safe.thundercore.com'
 
 // Magic numbers
 export const POLLING_INTERVAL = 15_000
